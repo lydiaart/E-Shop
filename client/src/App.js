91f0c718f9cba38735632277de4e3576d44aa3ec
Lydia Loy <lydia_art@yahoo.com>
2022-02-19 12:22:15 -0600

    changed global state of useSelector, useDispatch to redux

--- client/src/App.js
+++ client/src/App.js
@@ -14,9 +14,10 @@ import NoMatch from './pages/NoMatch';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Nav from './components/Nav';
-import { StoreProvider } from './utils/GlobalState';
 import Success from './pages/Success';
 import OrderHistory from './pages/OrderHistory';
+import {Provider} from 'react-redux'
+import store from './utils/reduxStore'
 
 const httpLink = createHttpLink({
   uri: '/graphql',
@@ -40,9 +41,9 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
+      <Provider store={store}>
       <Router>
-        <div>
-          <StoreProvider>
+        <div>        
             <Nav />
             <Switch>
               <Route exact path="/" component={Home} />
@@ -53,9 +54,9 @@ function App() {
               <Route exact path="/products/:id" component={Detail} />
               <Route component={NoMatch} />
             </Switch>
-          </StoreProvider>
         </div>
       </Router>
+      </Provider>
     </ApolloProvider>
   );
 }
